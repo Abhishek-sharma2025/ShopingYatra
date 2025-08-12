f94d7aee3bef09df1aab9711c07c950487667d92
Abhishek-sharma2025 <abhisheks39734@gmail.com>
2025-08-12 11:40:31 +0530

    Edit in category

--- src/components/categories/Category.js
+++ src/components/categories/Category.js
@@ -5,6 +5,7 @@ import { CategoryContext } from "../../App";
 function Category(){
     let {categoryList} = useContext(CategoryContext);
     return <>
+    
       <div className="container-fluid pt-5">
         <h2 className="section-title position-relative text-uppercase mx-xl-5 mb-4"><span className="bg-secondary pr-3">Categories</span></h2>
         <div className="row px-xl-5 pb-3">
